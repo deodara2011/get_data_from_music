865b8da130f48b494a5654a2973c6f9db57f0c9c
jiasl <jiasl@jiasl-THINK>
2018-09-19 21:36:51 +0800

    test

--- MusicTest/src/test/AutoBoxTest.java
+++ MusicTest/src/test/AutoBoxTest.java
@@ -6,7 +6,6 @@ public class AutoBoxTest {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        //test
         Integer a = 1;
         Integer b = 2;
         Integer c = 3;
